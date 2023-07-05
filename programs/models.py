ad84ecc060b4d375f7c38b630675aee61b90eae8
Xomak <ily30014001@gmail.com>
2023-07-05 23:27:07 +0700

    add program models

--- programs/models.py
+++ programs/models.py
@@ -32,18 +32,62 @@ class Program(models.Model):
         verbose_name_plural = 'Программы тренировок'
 
 
+class MuscleGroup(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
+    name = models.CharField(verbose_name='Название', unique=True)
+
+    class Meta:
+        verbose_name = 'Группа мышц'
+        verbose_name_plural = 'Группы мышц'
+
+
+class Exercise(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
+    muscle_group = models.ForeignKey('MuscleGroup', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Название', unique=True)
+
+    class Meta:
+        verbose_name = 'Упражнение'
+        verbose_name_plural = 'Упражнения'
+
+
 class Week(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
 
+    class Meta:
+        verbose_name = 'Неделя'
+        verbose_name_plural = 'Недели'
 
-class Workout(models.Model):  # тренировка
+
+class Workout(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
+    status = models.BooleanField(verbose_name='Статус (завершена/не завершена)', default=False)
+    week = models.ForeignKey('Week', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Тренировка'
+        verbose_name_plural = 'Тренировки'
 
 
-class MuscleGroup(models.Model):  # группа мышц
+class TrainingExercise(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
-
-
-class Exercise(models.Model):  # упражнение
-    id = models.UUIDField(primary_key=True, unique=True, db_index=True, default=uuid.uuid4)
-    muscle_group = models.ForeignKey('MuscleGroup', on_delete=models.CASCADE)
+    exercise = models.ForeignKey('Exercise', on_delete=models.CASCADE)
+    workout = models.ForeignKey('Workout', on_delete=models.CASCADE)
+    number_repetitions = models.IntegerField(
+        verbose_name='Количество повторений',
+        validators=[
+            validators.MinValueValidator(0),
+        ]
+    )
+    warmup_approach = models.IntegerField(
+        verbose_name='Количество разминочных подходов',
+        validators=[
+            validators.MinValueValidator(0),
+        ]
+    )
+    working_approach = models.IntegerField(
+        verbose_name='Количество рабочих подходов',
+        validators=[
+            validators.MinValueValidator(0),
+        ]
+    )
